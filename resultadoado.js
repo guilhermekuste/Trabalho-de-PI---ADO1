99d623fe6184dec149d9fe24eb36eb5967cd6a84
Guilherme Costa <guilhermekuste@gmail.com>
2026-09-08 11:45:09 -0300

    Update resultadoado.js

--- resultadoado.js
+++ resultadoado.js
@@ -1,15 +1,15 @@
 import { Leao } from "./LuisGuilhermeADO1.js";
-const leao1 = new leao("luis");
-const leao2 = new leao("lucas");
+const leao1 = new Leao("luis");
+const leao2 = new Leao("lucas");
 
 
 
-console.log("O leão 1 se chama " + leao1.nome);
+console.log("O leão 1 se chama " +leao1.nome);
 leao1.comer(8);
 leao1.dormir(10);
 
 ///////////////////////
 
-console.log("O leão 2 se chama " + leao2.nome);
+console.log("O leão 2 se chama " +leao2.nome);
 leao2.comer(1);
 leao2.dormir(2);
